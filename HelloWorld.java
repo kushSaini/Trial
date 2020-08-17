6e63646331cfd3ffd3b81fa1737d37d74195621d
kushSaini <69613422+kushSaini@users.noreply.github.com>
2020-08-17 10:26:17 +0530

    Scholars List Added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,14 @@
 package trial
 
+import java.util.*;
 public class HelloWorld{
 
   public static void main(String[] args){
     System.out.println("Hello and Welcome to GITHUB");
+    System.out.println("SCHOLAR's Names");
+    List<String> list=new LinkedList<>();
+    list.add("Kush Saini");
+    list.add("Rohan Nanda");
   }
 
 }
